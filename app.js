c2aaeb00d36acb4b16e1cd85fc978a33dad6b20c
AMOGH <amoghp549@gmail.com>
2022-11-11 23:00:42 +0530

    adding javascript and basic modification.

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const bars = document.querySelectorAll(".bar")
+const bars = document.querySelectorAll(".bar-graph")
 const amounttags = document.querySelectorAll(".amount-tag")
 
 for(let bar of bars){
     for(let amounttag of amounttags){
-    bar.addEventListener('mouseover',function()
-    {
-       amounttag.classList.remove('hidden')
-    })
-}
+        if(bar.contains(amounttag)==true){
+            bar.addEventListener('mouseenter',function(){
+                amounttag.classList.remove('hidden')
+            })
+            bar.addEventListener('mouseleave',function(){
+                amounttag.classList.add('hidden')
+            })
+        }
+    }
+   
 }
