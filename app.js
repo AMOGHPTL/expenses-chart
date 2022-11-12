531c01a9e86ac42af9ae3729068005ed7bb89bbd
AMOGH <amoghp549@gmail.com>
2022-11-12 23:05:18 +0530

    light and dark theme.

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const bars = document.querySelectorAll(".bar-graph")
 const amounttags = document.querySelectorAll(".amount-tag")
+const logo = document.querySelector(".logo")
+const topbar = document.querySelector(".top-bar")
+const amount = document.querySelector(".amount")
+const type = document.querySelectorAll(".type")
+const logotype = document.querySelector(".logo-type")
+const statsheading = document.querySelector(".stats-heading")
+const days = document.querySelectorAll('.day')
+const typeblack = document.querySelector(".type--black")
+const endbar = document.querySelector(".end-bar")
+const moneybig = document.querySelector(".money-big")
+const from = document.querySelector(".from")
+const main = document.querySelector(".main")
 
 for(let bar of bars){
     for(let amounttag of amounttags){
@@ -14,3 +26,31 @@ for(let bar of bars){
     }
    
 }
+
+logo.addEventListener('click',function(){
+    amount.classList.toggle('dark-letter')
+    for(let types of type){
+        types.classList.toggle('dark-letter')
+    }
+    logotype.classList.toggle('dark-letter')
+    if(logotype.classList.contains('dark-letter')==true){
+        logotype.innerText='Dark'
+    }
+    if(logotype.classList.contains('dark-letter')==false){
+        logotype.innerText='Light'
+    }
+    statsheading.classList.toggle('light-letter')
+    for(let day of days){
+        day.classList.toggle('light-letter')
+    }
+    typeblack.classList.toggle('light-letter')
+    endbar.classList.toggle('light-letter')
+    moneybig.classList.toggle('light-letter')
+    from.classList.toggle('light-letter')
+    for(let amounttag of amounttags){
+        amounttag.classList.toggle('dark-letter')
+        amounttag.classList.toggle('light')
+    }
+    main.classList.toggle('dark')
+    endbar.classList.toggle('dark')
+})
